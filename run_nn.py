51738e23ea04aa0c3a4dfbce87841c1b44a01153
slack0 <smuddu@gmail.com>
2017-05-10 17:48:36 -0700

    Added accuracy score on test set

--- run_nn.py
+++ run_nn.py
@@ -3,13 +3,13 @@ from src.layers import DenseLayer, InputLayer
 from src.utilities import load_data, sigmoid, softplus, softmax, tanh, relu
 
 input_layer = InputLayer((None, 784))
-hidden_layer = DenseLayer(input_layer, 30, nonlinearity=softplus)
+hidden_layer = DenseLayer(input_layer, 100, nonlinearity=relu)
 output_layer = DenseLayer(hidden_layer, 10, nonlinearity=softmax)
 layers = [input_layer, hidden_layer, output_layer]
 
 _mini_batch_size = 100
 _lr = 0.01
-_n_epochs = 15
+_n_epochs = 30
 
 nn = NeuralNet(layers,
                batch_size=_mini_batch_size,
@@ -21,3 +21,6 @@ train, valid, test = load_data('mnist.pkl.gz', True)
 nn.describe()
 
 nn.fit(train, valid, max_iters=_n_epochs)
+
+print 'test accuracy = {}'.format(nn.score(nn.eval(test[0]), test[1]))
+
